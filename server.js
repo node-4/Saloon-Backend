d3d55db6309fc6007bdca6b25dfbdea1fd07758b
rubiyadav <rubiflyeis@gmail.com>
2024-02-27 11:27:50 +0530

    update

--- server.js
+++ server.js
@@ -41,4 +41,5 @@ app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}!`);
 });
 
-module.exports = { handler: serverless(app) };
+module.exports = app;
+module.exports.handler = serverless(app);
